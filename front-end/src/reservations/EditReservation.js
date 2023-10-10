4e2cef04fd58c05ddb3f980160466a71de1d1d34
Kyle Haesler <kyle.haesler@gmail.com>
2023-10-10 12:34:37 -0400

    All tests passing: edit and create now share same form component!

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -5,6 +5,7 @@ import { getReservation } from "../utils/api";
 import { formatAsDate } from "../utils/date-time";
 import { formatAsTime } from "../utils/date-time";
 import { editReservation } from "../utils/api";
+import ReservationForm from "./ReservationForm";
 
 function EditReservation(){
     const [reservation, setReservation] = useState([])
@@ -64,94 +65,7 @@ function EditReservation(){
     };
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="first_name">
-                First Name:
-                <input 
-                id="first_name"
-                type="text"
-                name="first_name"
-                onChange={handleChange}
-                value={formData.first_name}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="last_name">
-                Last Name:
-                <input 
-                id="last_name"
-                type="text"
-                name="last_name"
-                onChange={handleChange}
-                value={formData.last_name}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="mobile_number">
-                Mobile Number:
-                <input 
-                id="mobile_number"
-                type="text"
-                name="mobile_number"
-                onChange={handleChange}
-                value={formData.mobile_number}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="reservation_date">
-                Reservation Date:
-                <input 
-                id="reservation_date"
-                type="date"
-                name="reservation_date"
-                onChange={handleChange}
-                value={formData.reservation_date}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="reservation_time">
-                Reservation Time:
-                <input 
-                id="reservation_time"
-                type="time"
-                name="reservation_time"
-                onChange={handleChange}
-                value={formData.reservation_time}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="people">
-                Party Size:
-                <input 
-                id="people"
-                type="number"
-                name="people"
-                onChange={handleChange}
-                value={formData.people}
-                required
-                min="1"
-                />
-            </label>
-            <br />
-            <label htmlFor="status">
-                Status:
-                <input 
-                id="status"
-                type="text"
-                name="status"
-                onChange={handleChange}
-                value={formData.status}
-                required
-                />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <ReservationForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit} />
         <button type="button" onClick={handleCancel}>Cancel</button>
         <ErrorAlert error={reservationError} />
         <ErrorAlert error={updatedResError} />
--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {useHistory} from "react-router-dom"
 import { createReservation } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import ReservationForm from "./ReservationForm";
 
 function NewReservation(){
     const [reservationsError, setReservationsError] = useState(null);
@@ -44,82 +45,7 @@ function NewReservation(){
     };
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="first_name">
-                First Name:
-                <input 
-                id="first_name"
-                type="text"
-                name="first_name"
-                onChange={handleChange}
-                value={formData.first_name}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="last_name">
-                Last Name:
-                <input 
-                id="last_name"
-                type="text"
-                name="last_name"
-                onChange={handleChange}
-                value={formData.last_name}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="mobile_number">
-                Mobile Number:
-                <input 
-                id="mobile_number"
-                type="text"
-                name="mobile_number"
-                onChange={handleChange}
-                value={formData.mobile_number}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="reservation_date">
-                Reservation Date:
-                <input 
-                id="reservation_date"
-                type="date"
-                name="reservation_date"
-                onChange={handleChange}
-                value={formData.reservation_date}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="reservation_time">
-                Reservation Time:
-                <input 
-                id="reservation_time"
-                type="time"
-                name="reservation_time"
-                onChange={handleChange}
-                value={formData.reservation_time}
-                required
-                />
-            </label>
-            <br />
-            <label htmlFor="people">
-                Party Size:
-                <input 
-                id="people"
-                type="number"
-                name="people"
-                onChange={handleChange}
-                value={formData.people}
-                required
-                min="1"
-                />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <ReservationForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit} />
         <button type="button" onClick={handleCancel}>Cancel</button>
         <ErrorAlert error={reservationsError} />
         </div>
